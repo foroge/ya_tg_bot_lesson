121af86a32f94c9cbf0a28c32978c12bf0de8c52
foroge <forog-en@yandex.ru>
2024-03-09 15:18:33 +0400

    set timer

--- main.py
+++ main.py
@@ -32,6 +32,45 @@ async def date_get(update, context):
     await update.message.reply_text(" ".join(time.asctime().split()[:3]))
 
 
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+# Обычный обработчик, как и те, которыми мы пользовались раньше.
+async def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    chat_id = update.effective_message.chat_id
+    # Добавляем задачу в очередь
+    # и останавливаем предыдущую (если она была)
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    time_value = int(context.args[0])
+    context.job_queue.run_once(task, time_value, chat_id=chat_id, name=str(chat_id), data=time_value)
+
+    text = f'Вернусь через {time_value} с.!'
+    if job_removed:
+        text += ' Старая задача удалена.'
+    await update.effective_message.reply_text(text)
+
+
+async def task(context):
+    """Выводит сообщение"""
+    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! время вышло!')
+
+
+async def unset(update, context):
+    """Удаляет задачу, если пользователь передумал"""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    await update.message.reply_text(text)
+
 def main():
     # Создаём объект Application.
     # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
@@ -48,6 +87,8 @@ def main():
     application.add_handler(text_handler)
     application.add_handler(CommandHandler("time", time_get))
     application.add_handler(CommandHandler("date", date_get))
+    application.add_handler(CommandHandler("set_timer", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
 
     # Запускаем приложение.
     application.run_polling()
